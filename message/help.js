8368ce770b459dac37488c2a019663221e974ae4
NANDO BOTz <53708431+winorohadimukti@users.noreply.github.com>
2022-02-28 02:59:27 +0700

    Add files via upload

--- message/help.js
+++ message/help.js
@@ -8,39 +8,6 @@ exports.menu = (ucapan, pushname, ownerName, botName, tanggal, jam, runtime, pre
 ⏳ Runtime
 ${runtime}
 
-*┏━━⬣「 *SIMPLE MENU* 」
-┃❏ ${prefix}sticker
-┃❏ ${prefix}attp
-┃❏ ${prefix}nulis
-┗━━⬣「 MENU LOL 」
-
-*┏━━⬣「 *GROUP MENU* 」
-┃❏ ${prefix}kick
-┃❏ ${prefix}welcome
-┃❏ ${prefix}left
-┃❏ ${prefix}setwelcome
-┃❏ ${prefix}changewelcome
-┃❏ ${prefix}delsetwelcome
-┃❏ ${prefix}setleft
-┃❏ ${prefix}changeleft
-┃❏ ${prefix}delsetleft
-┃❏ ${prefix}mute
-┃❏ ${prefix}hidetag
-┃❏ ${prefix}opengrup
-┃❏ ${prefix}closegrup
-┃❏ ${prefix}antilink
-┃❏ ${prefix}antiwame
-┃❏ ${prefix}checksewa
-┃❏ ${prefix}setcmd
-┃❏ ${prefix}delcmd
-┗━━⬣「 GERUP MENU OK 」
-
-*┏━━⬣「 *DOWNLOAD MENU* 」
-┃❏ ${prefix}instagram <error>
-┃❏ ${prefix}youtube <error>
-┃❏ ${prefix}tiktok
-┗━━⬣「 DOWNLOD VIDEO 」
-
 *┏━━⬣「 *STORE MENU* 」
 ┃❏ ${prefix}addlist
 ┃❏ ${prefix}dellist
@@ -49,19 +16,5 @@ ${runtime}
 ┃❏ p < reply orderan >
 ┃❏ d < reply orderan >
 ┗━━⬣「 LIS TOP UP GAME 」
-
-*┏━━⬣「 *OWNER MENU* 」
-┃❏ ${prefix}bc
-┃❏ ${prefix}setlogo
-┃❏ ${prefix}setprefix
-┃❏ ${prefix}exif
-┃❏ ${prefix}public
-┃❏ ${prefix}self
-┃❏ ${prefix}ban
-┃❏ ${prefix}unban
-┃❏ ${prefix}join
-┃❏ ${prefix}addsewa
-┃❏ ${prefix}delsewa
-┗━━⬣「 GONFOL BOTz 」
 `
 }
